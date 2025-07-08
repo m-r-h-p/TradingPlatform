9e1a2c805c0c991c181e5aec337b14845351f265
AmirHp <amir.hpid@gmail.com>
2025-07-08 16:55:49 +0330

    Add migration

--- TradingApp/TradingPlatform.API/Program.cs
+++ TradingApp/TradingPlatform.API/Program.cs
@@ -1,17 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using TradingPlatform.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ TradingApp/TradingPlatform.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TradingPlatform.Core.Entities;
+
+namespace TradingPlatform.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<User> users { get; set; }
+        public DbSet<Trade> Trades { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Trade>()
+                 .HasOne(t => t.User)
+                 .WithMany()
+                 .HasForeignKey(t => t.UserId);
+        }
+    }
+}
