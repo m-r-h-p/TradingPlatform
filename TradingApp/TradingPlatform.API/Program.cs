3d0ca54da16dccc71437e03ddad7df55554b26ab
AmirHp <amir.hpid@gmail.com>
2025-07-10 19:09:57 +0330

    Register jwt  & Service

--- TradingApp/TradingPlatform.API/Program.cs
+++ TradingApp/TradingPlatform.API/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.EntityFrameworkCore;
 using TradingPlatform.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TradingPlatform.Application.Services.Implementations;
+using TradingPlatform.Application.Services.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,11 +13,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IJwtService, JwtService>();
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
+
+//add jwt
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+//****
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
